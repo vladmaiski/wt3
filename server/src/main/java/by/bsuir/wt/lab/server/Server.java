a770fe8abe17892bd9c4f7d000231988aa577323
Vladislav Maiski <vladmaiski228@gmail.com>
2021-11-22 04:33:19 +0300

    InitialCommit

--- server/src/main/java/by/bsuir/wt/lab/server/Server.java
+++ server/src/main/java/by/bsuir/wt/lab/server/Server.java
@@ -4,7 +4,7 @@ package by.bsuir.wt.lab.server;
 import by.bsuir.wt.lab.server.controller.ServerController;
 
 public class Server {
-w
+
     public static void main(String[] args) {
         ServerController server = new ServerController();
         server.start();
